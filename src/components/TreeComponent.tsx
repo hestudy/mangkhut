f48ad793e68c87ef96f35b0b7e2254092e505113
hestudy <1467602180@qq.com>
2024-02-22 22:40:31 +0800

    添加了useUpdateEffect钩子和react-spring库来实现动画效果

--- src/components/TreeComponent.tsx
+++ src/components/TreeComponent.tsx
@@ -1,7 +1,8 @@
-import { useBoolean } from "ahooks";
+import { useBoolean, useUpdateEffect } from "ahooks";
 import { TreeData } from "../types";
 import { FolderIcon } from "./icons/FolderIcon";
 import { RightIcon } from "./icons/RightIcon";
+import { useSpring, animated } from "@react-spring/web";
 
 const TreeComponent = ({
   data,
@@ -10,36 +11,78 @@ const TreeComponent = ({
   data: TreeData[];
   level?: number;
 }) => {
-  const [open, openAc] = useBoolean(level === 0 ? true : false);
-
   return (
     <div>
       {data.map((item) => {
-        return (
-          <div key={item.id}>
-            <div
-              className="flex items-center mb-2 py-2 px-2 space-x-1 cursor-pointer hover:bg-gray-100"
-              style={{
-                paddingLeft: `${level * 20}px`,
-              }}
-            >
-              <RightIcon
-                fontSize={18}
-                className={`text-gray-600 ${
-                  item.children.length !== 0 ? "opacity-100" : "opacity-0"
-                }`}
-              ></RightIcon>
-              <FolderIcon fontSize={18} className="text-gray-600"></FolderIcon>
-              <div className="text-sm">{item.title}</div>
-            </div>
-            <div>
-              <TreeComponent data={item.children} level={level + 1} />
-            </div>
-          </div>
-        );
+        return <TreeItem item={item} level={level}></TreeItem>;
       })}
     </div>
   );
 };
 
 export default TreeComponent;
+
+const TreeItem = ({ item, level = 0 }: { item: TreeData; level?: number }) => {
+  const [open, openAc] = useBoolean(false);
+  const [spring, api] = useSpring(() => {
+    return {
+      from: {
+        display: "none",
+      },
+    };
+  });
+
+  useUpdateEffect(() => {
+    if (open) {
+      api.start({
+        from: {
+          display: "none",
+        },
+        to: {
+          display: "block",
+        },
+      });
+    } else {
+      api.start({
+        from: {
+          display: "block",
+        },
+        to: {
+          display: "none",
+        },
+      });
+    }
+  }, [open]);
+
+  return (
+    <div>
+      <div
+        className="flex items-center mb-2 py-2 px-2 space-x-1 cursor-pointer hover:bg-gray-100 select-none"
+        style={{
+          paddingLeft: `${level * 20}px`,
+        }}
+      >
+        <RightIcon
+          fontSize={18}
+          className={`text-gray-600 ${
+            item.children.length !== 0 ? "opacity-100" : "opacity-0"
+          }`}
+          onClick={() => {
+            if (item.children.length !== 0) {
+              openAc.toggle();
+            }
+          }}
+        ></RightIcon>
+        <FolderIcon fontSize={18} className="text-gray-600"></FolderIcon>
+        <div className="text-sm">{item.title}</div>
+      </div>
+      <animated.div
+        style={{
+          ...spring,
+        }}
+      >
+        <TreeComponent data={item.children} level={level + 1} />
+      </animated.div>
+    </div>
+  );
+};
